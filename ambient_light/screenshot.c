5e4f2bad5619564438afdd9bbeb51d100194aa8c
GRayHook <s@marinkevich.ru>
2017-09-09 14:48:47 +0700

    Add simple algorithm

--- ambient_light/screenshot.c
+++ ambient_light/screenshot.c
@@ -2,34 +2,79 @@
 
 int main() {
 
-  Display *display;
   XImage *image;
-
-  // Connect with Xserver
+  XColor color;
   if ((display = XOpenDisplay(getenv("DISPLAY"))) == NULL)
   {
     perror("Connect");
     exit(1);
   }
 
-  image = XGetImage(display,
-                    DefaultRootWindow(display), 0, 0,
-                    DISPLAY_WIDTH,
-                    DISPLAY_HEIGHT,
-                    AllPlanes,
-                    ZPixmap);
 
-  XColor color;
-  color.pixel = XGetPixel(image, 500, 500);
-  XQueryColor (display,
-               DefaultColormap(display, DefaultScreen (display)),
-               &color);
+  while (1) {
+    printf("Started!\n");
+    image = XGetImage(
+      display,
+      DefaultRootWindow(display), 0, 0,
+      DISPLAY_WIDTH,
+      DISPLAY_HEIGHT,
+      AllPlanes,
+      ZPixmap
+    );
+    color = main_color(image);
+    XDestroyImage(image);
+    printf(
+      "%d - %d - %d\n",
+      color.red/C_DENOM,
+      color.green/C_DENOM,
+      color.blue/C_DENOM
+    );
+    nanosleep(&tw, &tr);
+  }
 
-
-  // Close connection with Xserver
   XCloseDisplay(display);
 
-  printf("%d - %d - %d\n", color.red/C_DENOM, color.green/C_DENOM, color.blue/C_DENOM);
 
   return 0;
+
+}
+
+XColor main_color(XImage *image) {
+  XColor tmp_clr, rslt_clr;
+  unsigned int x = 0,
+               y = 0,
+               pixels = 0;
+  unsigned long red = 0,
+                green = 0,
+                blue = 0;
+
+  while(1) {
+    tmp_clr.pixel = XGetPixel(image, x, y);
+    XQueryColor (
+      display,
+      DefaultColormap(display, DefaultScreen (display)),
+      &tmp_clr
+    );
+
+    red += tmp_clr.red/C_DENOM;
+    green += tmp_clr.green/C_DENOM;
+    blue += tmp_clr.blue/C_DENOM;
+
+    x += rand() % PIXELS_STEP;
+    if (x > DISPLAY_WIDTH - 1) {
+      x = rand() % PIXELS_STEP;
+      y += rand() % PIXELS_STEP;
+    }
+    if (y > DISPLAY_HEIGHT - 1) {
+      break;
+    } else {
+      pixels++;
+    }
+  }
+
+  rslt_clr.red = (red / pixels) * C_DENOM;
+  rslt_clr.green = (green / pixels) * C_DENOM;
+  rslt_clr.blue = (blue / pixels) * C_DENOM;
+
+  return rslt_clr;
 }
--- ambient_light/screenshot.h
+++ ambient_light/screenshot.h
@@ -2,13 +2,18 @@
 #include <X11/Xutil.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <time.h>
 //------------------------------------------------------------------------------------------------
-#define X 0
-#define Y 0
-#define WIDTH 600
-#define HEIGHT 300
 #define DISPLAY_WIDTH 1920
 #define DISPLAY_HEIGHT 1080
 #define C_DENOM 1024
+#define PIXELS_STEP 20
+#define  T_DELAY 400000000
 //------------------------------------------------------------------------------------------------
+Display *display;
+struct timespec tr;
+struct timespec tw = {0,T_DELAY};
+//------------------------------------------------------------------------------------------------
+XColor main_color(XImage *image);

d8c9dbf031de0ddd00d5c5b21a2160bdab4de4eb
GRayHook <s@marinkevich.ru>
2017-09-10 01:03:42 +0700

    + Clean drisnya + Add mediator for windows

---
+++ ambient_mediator/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
+
+#pragma comment(lib,"Ws2_32.lib")
+#pragma comment(lib,"advapi32.lib") // ЅЋяяяяяя“№ я  ј  ƒќ√јƒј“№—я ƒќЋ∆≈Ќ ЅџЋ Ќј’”… ѕ»ƒќ–џ јјјјјј
+#pragma comment (lib, "LogitechLEDLib.lib" )
+#include <winsock2.h>
+#include <windows.h>
